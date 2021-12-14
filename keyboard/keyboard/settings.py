87dd1dea8939095d3c3d4a4ef54c8e7a3613c9a6
Bahromiddin-0254 <bahromiddinibragimov03brr@gmail.com>
2021-12-14 08:50:02 +0500

    1

--- keyboard/keyboard/settings.py
+++ keyboard/keyboard/settings.py
@@ -124,3 +124,4 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'staticfiles'),)
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
